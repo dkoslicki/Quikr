b0884d426a90aa61029bf11e541171fd8167efe5
David Koslicki <dkoslicki@10-196-22-18.wireless.oregonstate.edu>
2014-10-08 10:54:53 -0700

    Added SEK training database option

--- src/Julia/Quikr.jl
+++ src/Julia/Quikr.jl
@@ -9,148 +9,7 @@
 using ArgParse
 using HDF5
 include("lsqnonneg.jl")
-
-#ConvertToCAMIOutput.jl as a function
-function ConvertToCAMIOutputFunction(input, taxonomy_file, output_level, output_file)
-	output_taxonomic_rank = [1:7]
-	sample_ID="SAMPLEID"
-
-	#Next, read in the taxonomy file
-	fid = open(taxonomy_file,"r")
-	taxonomy = readlines(fid)
-	close(fid)
-	taxonomy = map(x->strip(split(x)[3]), taxonomy)
-	num_organisms = length(taxonomy)
-	
-	#If the output_level is not 0, just do that level
-	if ~(output_level==0)
-	
-		#Just select the ones in the output level of interest
-		indicies_of_interest = ((output_level-1) * num_organisms + 1):(output_level * num_organisms)
-
-		#First, select the portion of the taxonomy that has a nonzero entry in the reconstruction
-		cutoff = .00001
-		support = indicies_of_interest[find(input[indicies_of_interest] .> cutoff)] #Support in the indicies of interest
-		nonzero_taxonomy = taxonomy[(support .- (num_organisms*(output_level-1)))] #Shift everything left to the start since taxonomy has only num_organisms length
-	elseif output_level==0
-		if ~(int(length(input)/num_organisms)==length(input)/num_organisms)
-			error("Input reconstruction is not a multiple of the number of organisms in the taxonomy file")
-		else
-			#Then add up each chunk of size num_organisms
-			input_temp = zeros(num_organisms);
-			for output_level_temp = 1:int(length(input)/num_organisms)
-				input_temp = input_temp + input[((output_level_temp-1) * num_organisms + 1):(output_level_temp * num_organisms)];
-			end
-			cutoff = .00001
-			support = find(input_temp .> cutoff) #Support in the indicies of interest
-			nonzero_taxonomy = taxonomy[support]
-		end
-	else
-		error("non-valid output_level")
-	end
-	#open the output file
-	output_file_handle = open(output_file,"w")
-	
-	#Write the header
-	write(output_file_handle,"# CAMI Submission for Taxonomic Profiling\n")
-	write(output_file_handle, "@Task:Profiling\n")
-	write(output_file_handle,"@Version:1.0\n")
-	write(output_file_handle,"@ContestantID:CONTESTANTID\n")
-	write(output_file_handle,"@SampleID:$(sample_ID)\n")
-	write(output_file_handle,"@Ranks: superkingdom|phylum|class|order|family|genus|species|strain\n")
-	write(output_file_handle,"\n")
-	write(output_file_handle,"@@TAXID\tRANK\tTAXPATH\tTAXPATH_SN\tPERCENTAGE\n")
-	
-	#Now for each of the ranks, get the unique names, then loop over the non-zero taxonomy, increasing the value of the unique taxa name, then output these to the file
-	if typeof(output_taxonomic_rank) == Int64
-		taxa_rank_list = [output_taxonomic_rank]
-	elseif typeof(output_taxonomic_rank) == Array{Int64,1}
-		taxa_rank_list = output_taxonomic_rank
-	else
-		error("Input taxonomic rank should be an integer, or else don't include the option to output all ranks")
-	end
-	
-	for taxa_rank = taxa_rank_list	
-		taxa_names = cell(0);
-		for taxonomy_string = nonzero_taxonomy
-			nonzero_taxonomy_split = split(taxonomy_string,"|")
-			if length(nonzero_taxonomy_split) >= taxa_rank
-				taxa_name = join(nonzero_taxonomy_split[1:taxa_rank],"|")
-				append!(taxa_names, {taxa_name})
-			end
-		end
-		unique_taxa_names = sort(unique(taxa_names)); #This assumes that there's a bijection between taxa names and tax IDs
-	
-		#Now loop through each of the non_zero taxonomies, see if the taxa name matches, and then add this to the abundances
-		taxa_abundances = Dict();
-		for unique_taxa_name = unique_taxa_names
-				taxa_abundances[unique_taxa_name] = 0
-		end
-		nonzero_taxonomy_counter = 1
-		for taxonomy_string = nonzero_taxonomy
-			nonzero_taxonomy_split = split(taxonomy_string,"|")
-			if length(nonzero_taxonomy_split) >= taxa_rank
-				taxa_name = join(nonzero_taxonomy_split[1:taxa_rank],"|")
-				taxa_abundances[taxa_name] = taxa_abundances[taxa_name] + input[support[nonzero_taxonomy_counter]]
-			end
-			nonzero_taxonomy_counter = nonzero_taxonomy_counter + 1
-		end
-		for unique_taxa_name = unique_taxa_names
-			taxID = split(split(unique_taxa_name,"|")[end],"_")[3];
-			write(output_file_handle, "$(taxID)")
-			write(output_file_handle, "\t")
-			
-			rankAbvr = split(split(unique_taxa_name,"|")[end],"_")[1];
-			if rankAbvr == "k"
-				rank = "superkingdom"
-			elseif rankAbvr == "p"
-				rank = "phylum"
-			elseif rankAbvr == "c"
-				rank = "class"
-			elseif rankAbvr == "o"
-				rank = "order"
-			elseif rankAbvr == "f"
-				rank = "family"
-			elseif rankAbvr == "g"
-				rank = "genus"
-			elseif rankAbvr == "s"
-				rank = "species"
-			elseif rankAbvr == "t"
-				rank = "string"
-			else
-				rank = "unknown"
-			end
-			write(output_file_handle, "$(rank)")
-			write(output_file_handle, "\t")
-			
-			taxPath = map(x->split(x,"_")[3],split(unique_taxa_name,"|")); #Tax ID's
-			taxPathSN = map(x->join(split(x,"_")[4:end],"_"),split(unique_taxa_name,"|")); #Taxa names
-		
-			#If a Tax ID is repeated at a lower taxonomic rank, this means that that rank is missing, so let's just delete it.
-			for i=1:length(taxPath)
-				if i>=2
-					if taxPath[i] == taxPath[i-1]
-						taxPath[i] = ""
-						taxPathSN[i] = ""
-					end
-				end
-			end
-			#Join back up the paths
-			taxPath = join(taxPath,"|")
-			taxPathSN = join(taxPathSN,"|")
-			#write the output
-			write(output_file_handle, "$(taxPath)")
-			write(output_file_handle, "\t")
-			write(output_file_handle, "$(taxPathSN)")
-			write(output_file_handle, "\t")
-			write(output_file_handle, "$(taxa_abundances[unique_taxa_name])")
-			write(output_file_handle, "\n")
-		end
-	end
-
-	#Close the output file
-	close(output_file_handle)
-end
+include("ConvertToCAMIOutput.jl")
 
 
 #Parse arguments
@@ -168,6 +27,9 @@ function parse_commandline()
 		"--kmer_count_path", "-k"
 			help = "Full path to the kmer_total_count file obtained from https://github.com/mutantturkey/dna-utils"
 			default = "kmer_total_count"
+		"--training_file", "-t"
+			help = "Training database to use. Choices are: SEK, Quikr. SEK is more accurate, but slower, whereas Quikr is faster, but less accurate. Default is Quikr"
+			default = "Quikr"
 			
     end
     return parse_args(s)
@@ -180,6 +42,7 @@ input_file = parsed_args["input_file"]
 lambda = parsed_args["lambda"]
 output_file = parsed_args["output_file"]
 kmer_count_path = parsed_args["kmer_count_path"]
+training_file = parsed_args["training_file"]
 
 #Form the 6mer counts
 counts = map(x->int(strip(x)),readlines(`$kmer_count_path -i $input_file -k 6 -c`));
@@ -187,25 +50,48 @@ counts = map(x->int(strip(x)),readlines(`$kmer_count_path -i $input_file -k 6 -c
 #normalize the counts
 counts = counts/sum(counts);
 
-#Read in the training database
-A = h5read("../../data/trainset7_112011N6C.h5","/data");
+if training_file == "Quikr"
+	#Read in the training database
+	A = h5read("../../data/trainset7_112011N6C.h5","/data");
 
-#Form the Aaux
-Aaux = [ones(1,size(A,2)); lambda*A];
-yaux = [0;lambda*counts];
+	#Form the Aaux
+	Aaux = [ones(1,size(A,2)); lambda*A];
+	yaux = [0;lambda*counts];
 
-#Perform the reconstruction
-x = lsqnonneg(Aaux, yaux);
+	#Perform the reconstruction
+	x = lsqnonneg(Aaux, yaux);
 
-#Normalize the output
-x = x/sum(x);
+	#Normalize the output
+	x = x/sum(x);
 
-#Write the output to file
-#fid = open(output_file,"w")
-#for i = 1:length(x)
-#	write(fid,"$(x[i])\n")
-#end
-ConvertToCAMIOutputFunction(x, "../../data/trainset7_taxonomy.txt", 0, output_file)
+	#Write the output to file
+	output_level = 0; #Since we don't have hypothetical organisms
+	ConvertToCAMIOutput(x, "../../data/trainset7_taxonomy.txt", output_level, output_file)
+elseif training_file = "SEK"
+	#Read in the training database
+	A = h5read("../../data/trainset7_112011_allseqslongerthan700-SEKTrainingMatrix-bitShift100-windowLength400-N6C.h5","/data");
+	
+	#Read in the block matrix to return to the trainset7_SEK basis
+	blockMatrix = h5read("../../data/trainset7_112011_allseqslongerthan700-SEKTrainingMatrix-bitShift100-windowLength400-blockMatrix.h5","/data");
+
+	#Form the Aaux
+	Aaux = [ones(1,size(A,2)); lambda*A];
+	yaux = [0;lambda*counts];
+
+	#Perform the reconstruction
+	x = lsqnonneg(Aaux, yaux);
+	
+	#Return to the trainset7_SEK basis
+	x = blockMatrix*x;
+
+	#Normalize the output
+	x = x/sum(x);
+
+	#Write the output to file
+	output_level = 0; #Since we don't have hypothetical organisms
+	ConvertToCAMIOutputFunction(x, "../../data/trainset7_SEK_taxonomy.txt", output_level, output_file)
+end
+
 
 
 
